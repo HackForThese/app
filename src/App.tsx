2b84f4af83d6156c503133c15aa920ccdc7d4e5a
fifamobilegems <max.limhy@gmail.com>
2026-01-14 14:06:59 +0800

    Initial setup for project

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+type Instrument = {
+  id: number;      // or whatever your PK is
+  name: string;
+};
+
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
+
+function App() {
+  const [instruments, setInstruments] = useState<Instrument[]>([]);
+
+  useEffect(() => {
+    getInstruments();
+  }, []);
+
+  async function getInstruments() {
+    const { data } = await supabase.from("instruments").select();
+    setInstruments(data ?? []);
+  }
+
+  return (
+    <ul>
+      {instruments.map((instrument) => (
+        <li key={instrument.name}>{instrument.name}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default App;
